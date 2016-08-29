557b1fa6c04c4eefff4f41b01676ae0d41a1c73e
aditimarathe <aditimarathe@hotmail.com>
2016-08-29 17:17:00 +0530

    Added validation

--- custom-popup-editor/KendoUIMVC5/Models/Person.cs
+++ custom-popup-editor/KendoUIMVC5/Models/Person.cs
@@ -9,6 +9,7 @@ namespace KendoUIMVC5.Models
     public class Person
     {
         public int PersonID { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public int Designation { get; set; } 
